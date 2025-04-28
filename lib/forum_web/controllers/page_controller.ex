38f1e63439a76f7ed9c40194090754314ab73340
HP <mintesinotbekele1240@gmail.com>
2025-04-28 06:01:09 +0300

    basic CRUD implementation

--- lib/forum_web/controllers/page_controller.ex
+++ lib/forum_web/controllers/page_controller.ex
@@ -6,4 +6,16 @@ defmodule ForumWeb.PageController do
     # so skip the default app layout.
     render(conn, :home, layout: false)
   end
+
+  def users(conn, _params) do
+
+    users = [
+      %{id: 1, name: "John", email: "john@gmail.com"},
+      %{id: 2, name: "Jane", email: "jane@gmail.com"}
+    ]
+
+    IO.puts("Users function hit!")
+    # render(conn, :users, users: users, layout: false)
+    json(conn, %{users: users})
+  end
 end
